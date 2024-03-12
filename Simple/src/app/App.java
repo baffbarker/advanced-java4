01045753f5d18dfe359264aa647717c5a96e23e7
Jeremy Stocks <jeremy.stocks@zoho.com>
2024-03-12 22:21:50 +0100

    Changed to 2

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -2,6 +2,6 @@ package app;
 
 class App {
     public static void main(String[] args) {
-        System.out.println("Helllo World 1 in master!");
+        System.out.println("Helllo World 2 in master!");
     }
 }
